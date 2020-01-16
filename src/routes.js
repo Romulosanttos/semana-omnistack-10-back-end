d401669260d790e985d8cea1fc2a35c9acc124fd
romulosanttos <romulosanttos1@gmail.com>
2020-01-16 03:27:48 -0300

    adicionando listagem de Devs

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
-const DevController =  require('./controllers/devController');
+const DevController = require("./controllers/devController");
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
   return res.json({ message: "Hello World" });
 });
-
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
 
 module.exports = routes;
